4a024c1b34e50152654b3c315519c8098c188080
jEETBUDHANI <sk0471011@gmail.com>
2025-04-27 17:00:33 +0530

    Initial project commit

--- frontend/postcss.config.js
+++ frontend/postcss.config.js
@@ -1,6 +1,14 @@
+<<<<<<< HEAD
 module.exports = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
   },
+=======
+export default {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  }
+>>>>>>> f81fff0 (Initial project commit)
 }
